0cff69c09c6c371a8669ea5886cf245ebd58fd88
Samuel Kinnett <samuelkinnett@member.fsf.org>
2018-01-17 18:00:54 +0000

    Added initial menu
    Began work on the main menu

---
+++ dissertation-game/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject MainMenuPanel;
+    public GameObject JoinGamePanel;
+    public GameObject HostGamePanel;
+    public GameObject OptionsPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SwitchToJoinGamePanel()
+    {
+
+    }
+
+    public void SwitchToHostGamePanel()
+    {
+
+    }
+
+    public void SwitchToOptionsPanel()
+    {
+
+    }
+
+    public void Quit()
+    {
+        
+    }
+}
