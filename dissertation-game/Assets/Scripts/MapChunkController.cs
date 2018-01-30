69b4d7277b6bf2d1e633d74f71ace92637906bcb
Samuel Kinnett <samuelkinnett@member.fsf.org>
2018-01-30 15:58:15 +0000

    Added chunk system
    Added a chunk system to the map, enabling much larger map sizes than
    previously possible.

---
+++ dissertation-game/Assets/Scripts/MapChunkController.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MapChunkController : MonoBehaviour
+{
+	[SerializeField] MeshFilter meshFilter;
+	[SerializeField] MeshCollider meshCollider;
+
+	private Mesh mesh;
+
+	private List<Vector3> newVertices;
+	private List<int> newTriangles;
+	private List<Vector2> newUV;
+
+	private float textureUnit = 0.25f;
+	private Vector2 textureStone = new Vector2(1, 0);
+	private Vector2 textureGrass = new Vector2(0, 1);
+
+	private int faceCount;
+
+	private int chunkX;
+	private int chunkY;
+	private int chunkZ;
+	private int chunkWidth;
+	private int chunkHeight;
+	private int chunkLength;
+
+	private bool initialised;
+	private MapController mapController;
+
+	public void Initialise(
+		int chunkX,
+		int chunkY,
+		int chunkZ,
+		int chunkWidth,
+		int chunkHeight,
+		int chunkLength)
+	{
+		this.chunkX = chunkX;
+		this.chunkY = chunkY;
+		this.chunkZ = chunkZ;
+		this.chunkWidth = chunkWidth;
+		this.chunkHeight = chunkHeight;
+		this.chunkLength = chunkLength;
+
+		mapController = this.GetComponentInParent<MapController>();
+
+		initialised = true;
+	}
+
+	public void GenerateMesh()
+	{
+		for (int x = chunkX; x < chunkX + chunkWidth; ++x) {
+			for (int y = chunkY; y < chunkY + chunkHeight; ++y) {
+				for (int z = chunkZ; z < chunkZ + chunkLength; ++z) {
+					byte curBlock = mapController.GetBlock(x, y, z);
+
+					// If this block is solid
+					if (curBlock > 0) {
+						// Compare the block to all of its neighbours. Any side that faces air should
+						// have a face rendered.
+
+						if (mapController.GetBlock(x, y + 1, z) == 0) {
+							CreateCubeTopFace(x, y, z, curBlock);
+						}
+
+						if (mapController.GetBlock(x, y, z + 1) == 0) {
+							CreateCubeNorthFace(x, y, z, curBlock);
+						}
+
+						if (mapController.GetBlock(x + 1, y, z) == 0) {
+							CreateCubeEastFace(x, y, z, curBlock);
+						}
+
+						if (mapController.GetBlock(x, y, z - 1) == 0) {
+							CreateCubeSouthFace(x, y, z, curBlock);
+						}
+
+						if (mapController.GetBlock(x - 1, y, z) == 0) {
+							CreateCubeWestFace(x, y, z, curBlock);
+						}
+
+						if (mapController.GetBlock(x, y - 1, z) == 0) {
+							CreateCubeBottomFace(x, y, z, curBlock);
+						}
+					}
+				}
+			}
+		}
+
+		UpdateMesh();
+	}
+
+	private void Awake()
+	{
+		initialised = false;
+
+		mesh = meshFilter.mesh;
+
+		newVertices = new List<Vector3>();
+		newTriangles = new List<int>();
+		newUV = new List<Vector2>();
+	}
+
+	private void UpdateMesh()
+	{
+		// Clear the mesh and set the vertices, UVs and triangles
+		mesh.Clear();
+		mesh.SetVertices(newVertices);
+		mesh.SetUVs(0, newUV);
+		mesh.SetTriangles(newTriangles, 0);
+
+		// Optimise the mesh
+		MeshUtility.Optimize(mesh);
+
+		// Recalculate normals
+		mesh.RecalculateNormals();
+
+		// Create the collision mesh
+		meshCollider.sharedMesh = mesh;
+
+		// Clear the buffers
+		newVertices.Clear();
+		newUV.Clear();
+		newTriangles.Clear();
+
+		// Reset the face count
+		faceCount = 0;
+	}
+
+	private void CreateCubeTopFace(int x, int y, int z, byte block)
+	{
+		// Add the new vertices
+		newVertices.Add(new Vector3(x, y, z));
+		newVertices.Add(new Vector3(x, y, z + 1));
+		newVertices.Add(new Vector3(x + 1, y, z + 1));
+		newVertices.Add(new Vector3(x + 1, y, z));
+
+		ApplyTextureToFace(textureStone);
+	}
+
+	private void CreateCubeNorthFace(int x, int y, int z, byte block)
+	{
+		// Add the new vertices
+		newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
+		newVertices.Add(new Vector3(x + 1, y, z + 1));
+		newVertices.Add(new Vector3(x, y, z + 1));
+		newVertices.Add(new Vector3(x, y - 1, z + 1));
+
+		ApplyTextureToFace(textureStone);
+	}
+
+	private void CreateCubeEastFace(int x, int y, int z, byte block)
+	{
+		// Add the new vertices
+		newVertices.Add(new Vector3(x + 1, y - 1, z));
+		newVertices.Add(new Vector3(x + 1, y, z));
+		newVertices.Add(new Vector3(x + 1, y, z + 1));
+		newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
+
+		ApplyTextureToFace(textureStone);
+	}
+
+	private void CreateCubeSouthFace(int x, int y, int z, byte block)
+	{
+		// Add the new vertices
+		newVertices.Add(new Vector3(x, y - 1, z));
+		newVertices.Add(new Vector3(x, y, z));
+		newVertices.Add(new Vector3(x + 1, y, z));
+		newVertices.Add(new Vector3(x + 1, y - 1, z));
+
+		ApplyTextureToFace(textureStone);
+	}
+
+	private void CreateCubeWestFace(int x, int y, int z, byte block)
+	{
+		// Add the new vertices
+		newVertices.Add(new Vector3(x, y - 1, z + 1));
+		newVertices.Add(new Vector3(x, y, z + 1));
+		newVertices.Add(new Vector3(x, y, z));
+		newVertices.Add(new Vector3(x, y - 1, z));
+
+		ApplyTextureToFace(textureStone);
+	}
+
+	private void CreateCubeBottomFace(int x, int y, int z, byte block)
+	{
+		// Add the new vertices
+		newVertices.Add(new Vector3(x, y - 1, z));
+		newVertices.Add(new Vector3(x + 1, y - 1, z));
+		newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
+		newVertices.Add(new Vector3(x, y - 1, z + 1));
+
+		ApplyTextureToFace(textureStone);
+	}
+
+	private void ApplyTextureToFace(Vector2 texturePosition)
+	{
+		// Generate the triangles
+		var offset = faceCount * 4;
+
+		newTriangles.Add(offset);		// 1
+		newTriangles.Add(offset + 1); 	// 2
+		newTriangles.Add(offset + 2);	// 3
+		newTriangles.Add(offset);
+		newTriangles.Add(offset + 2);
+		newTriangles.Add(offset + 3);
+
+		var xOrigin = textureUnit * texturePosition.x;
+		var yOrigin = textureUnit * texturePosition.y;
+
+		newUV.Add(new Vector2(xOrigin, yOrigin));
+		newUV.Add(new Vector2(xOrigin + textureUnit, yOrigin));
+		newUV.Add(new Vector2(xOrigin + textureUnit, yOrigin + textureUnit));
+		newUV.Add(new Vector2(xOrigin, yOrigin + textureUnit));
+
+		++faceCount;
+	}
+}
--- dissertation-game/Assets/Scripts/MapController.cs
+++ dissertation-game/Assets/Scripts/MapController.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 public class MapController : MonoBehaviour
 {
-	[SerializeField] MeshFilter meshFilter;
-	[SerializeField] MeshCollider meshCollider;
-
 	// The X, Y and Z dimensions of the map respectively
 	public int mapWidth;
-	public int mapLength;
 	public int mapHeight;
+	public int mapLength;
+
+	// The X, Y and Z dimensions of the chunks
+	public int chunkWidth;
+	public int chunkHeight;
+	public int chunkLength;
+
+	// The map chunk prefab
+	public GameObject mapChunkPrefab;
 
 	// This array stores the data of the map geometry, with each element representing one 'block'.
 	// Current possible values are:
@@ -20,115 +26,9 @@ public class MapController : MonoBehaviour
 	// Could eventually add different terrain types here, even if only for graphical effect.
 	private byte[,,] mapData;
 
-	private Mesh mesh;
+	private List<MapChunkController> mapChunks;
 
-	private List<Vector3> newVertices;
-	private List<int> newTriangles;
-	private List<Vector2> newUV;
-
-	private float textureUnit = 0.25f;
-	private Vector2 textureStone = new Vector2(1, 0);
-	private Vector2 textureGrass = new Vector2(0, 1);
-
-	private int faceCount;
-
-	// Use this for initialization
-	private void Start()
-	{
-		mapData = new byte[mapWidth, mapHeight, mapLength];
-
-		// Add a testing plane to the map
-		for (int x = 0; x < mapWidth; ++x) {
-			for (int z = 0; z < mapLength; ++z) {
-				mapData[x, 0, z] = 1;
-			}
-		}
-
-		mesh = meshFilter.mesh;
-
-		newVertices = new List<Vector3>();
-		newTriangles = new List<int>();
-		newUV = new List<Vector2>();
-
-		GenerateMesh();
-	}
-	
-	// Update is called once per frame
-	private void Update()
-	{
-		
-	}
-
-	private void UpdateMesh()
-	{
-		// Clear the mesh and set the vertices, UVs and triangles
-		mesh.Clear();
-		mesh.SetVertices(newVertices);
-		mesh.SetUVs(0, newUV);
-		mesh.SetTriangles(newTriangles, 0);
-
-		// Optimise the mesh
-		MeshUtility.Optimize(mesh);
-
-		// Recalculate normals
-		mesh.RecalculateNormals();
-
-		// Create the collision mesh
-		meshCollider.sharedMesh = mesh;
-
-		// Clear the buffers
-		newVertices.Clear();
-		newUV.Clear();
-		newTriangles.Clear();
-
-		// Reset the face count
-		faceCount = 0;
-	}
-
-	private void GenerateMesh()
-	{
-		for (int x = 0; x < mapWidth; ++x) {
-			for (int y = 0; y < mapHeight; ++y) {
-				for (int z = 0; z < mapLength; ++z) {
-					byte curBlock = GetBlock(x, y, z);
-
-					// If this block is solid
-					if (curBlock > 0) {
-						// Compare the block to all of its neighbours. Any side that faces air should
-						// have a face rendered.
-
-						if (GetBlock(x, y + 1, z) == 0) {
-							CreateCubeTopFace(x, y, z, curBlock);
-						}
-
-						if (GetBlock(x, y, z + 1) == 0) {
-							CreateCubeNorthFace(x, y, z, curBlock);
-						}
-
-						if (GetBlock(x + 1, y, z) == 0) {
-							CreateCubeEastFace(x, y, z, curBlock);
-						}
-
-						if (GetBlock(x, y, z - 1) == 0) {
-							CreateCubeSouthFace(x, y, z, curBlock);
-						}
-
-						if (GetBlock(x - 1, y, z) == 0) {
-							CreateCubeWestFace(x, y, z, curBlock);
-						}
-
-						if (GetBlock(x, y - 1, z) == 0) {
-							CreateCubeBottomFace(x, y, z, curBlock);
-						}
-					}
-				}
-			}
-		}
-
-		UpdateMesh();
-	}
-
-	private byte GetBlock(int x, int y, int z)
+	public byte GetBlock(int x, int y, int z)
 	{
 		if (x >= mapWidth ||
 		    x < 0 ||
@@ -142,92 +42,322 @@ public class MapController : MonoBehaviour
 		return mapData[x, y, z];
 	}
 
-	private void CreateCubeTopFace(int x, int y, int z, byte block)
+	// Use this for initialization
+	private void Start()
 	{
-		// Add the new vertices
-		newVertices.Add(new Vector3(x, y, z));
-		newVertices.Add(new Vector3(x, y, z + 1));
-		newVertices.Add(new Vector3(x + 1, y, z + 1));
-		newVertices.Add(new Vector3(x + 1, y, z));
+		mapData = new byte[mapWidth, mapHeight, mapLength];
+		mapChunks = new List<MapChunkController>();
 
-		ApplyTextureToFace(textureStone);
+		// Add a testing plane to the map
+		//for (int x = 0; x < mapWidth; ++x) {
+		//	for (int z = 0; z < mapLength; ++z) {
+		//		mapData[x, 0, z] = 1;
+		//	}
+		//}
+
+		GenerateWorld();
+		InstantiateChunks();
+		GenerateMesh();
 	}
 
-	private void CreateCubeNorthFace(int x, int y, int z, byte block)
+	private void InstantiateChunks()
 	{
-		// Add the new vertices
-		newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
-		newVertices.Add(new Vector3(x + 1, y, z + 1));
-		newVertices.Add(new Vector3(x, y, z + 1));
-		newVertices.Add(new Vector3(x, y - 1, z + 1));
+		int mapWidthInChunks = (int)Math.Ceiling((double)mapWidth / (double)chunkWidth);
+		int mapHeightInChunks = (int)Math.Ceiling((double)mapHeight / (double)chunkHeight);
+		int mapLengthInChunks = (int)Math.Ceiling((double)mapLength / (double)chunkLength);
 
-		ApplyTextureToFace(textureStone);
+		Debug.Log("Instantiating Chunks (" + mapWidthInChunks + ", " + mapHeightInChunks + ", " + mapLengthInChunks + ")");
+
+		for (int x = 0; x < mapWidthInChunks; ++x) {
+			for (int y = 0; y < mapHeightInChunks; ++y) {
+				for (int z = 0; z < mapLengthInChunks; ++z) {
+					// Instantiate a map chunk
+					var newChunk = Instantiate(mapChunkPrefab);
+					newChunk.transform.parent = this.transform;
+
+					// Initialise the chunk
+					var newChunkController = newChunk.GetComponent<MapChunkController>();
+					newChunkController.Initialise(x * chunkWidth, y * chunkHeight, z * chunkLength, chunkWidth, chunkHeight, chunkLength);
+
+					// Add the chunk to the list
+					mapChunks.Add(newChunkController);
+				}
+			}
+		}
+
+		Debug.Log("Chunks Instantiated");
 	}
 
-	private void CreateCubeEastFace(int x, int y, int z, byte block)
+	private void GenerateMesh()
 	{
-		// Add the new vertices
-		newVertices.Add(new Vector3(x + 1, y - 1, z));
-		newVertices.Add(new Vector3(x + 1, y, z));
-		newVertices.Add(new Vector3(x + 1, y, z + 1));
-		newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
-
-		ApplyTextureToFace(textureStone);
+		foreach (var currentChunk in mapChunks) {
+			currentChunk.GenerateMesh();
+		}
 	}
 
-	private void CreateCubeSouthFace(int x, int y, int z, byte block)
-	{
-		// Add the new vertices
-		newVertices.Add(new Vector3(x, y - 1, z));
-		newVertices.Add(new Vector3(x, y, z));
-		newVertices.Add(new Vector3(x + 1, y, z));
-		newVertices.Add(new Vector3(x + 1, y - 1, z));
+	#region testing
 
-		ApplyTextureToFace(textureStone);
+	/// <summary>
+	/// This method repeatedly uses cellular automata with decreasing probability to
+	/// generate land in order to build the terrain.
+	/// </summary>
+	private void GenerateWorld()
+	{
+		// int[,] heightMap = new int[worldWidth, worldHeight]; //this array stores the height of each chunk. 0 = bedrock layer, 1 - 8 = ocean, 9 - 31 = land
+		byte[,] temporaryMap = new byte[mapWidth, mapLength]; //this array is used in the generation process to create the layer before it is applied to the heightmap
+
+		int neighbourCount = 0; //this is used to store the number of 'alive' tiles surrounding a cell;
+
+		System.Random rand = new System.Random();
+
+		//fileManager.CreateWorldFolder(worldName);
+		double landChance = 0.78;
+
+		//fill the heightmap with bedrock (lowest layer)
+		for (int x = 0; x < mapWidth; ++x) {
+			for (int z = 0; z < mapLength; ++z) {
+				mapData[x, 0, z] = 1;
+			}
+		}
+
+		for (int y = 1; y < mapHeight - 1; ++y) { //for each height level
+			//clear the arrays of their previous contents
+			Array.Clear(temporaryMap, 0, temporaryMap.Length);
+
+			//firstly, fill most of the available area with random values (0 = empty space, 1 = filled).
+			for (int tempZ = 2; tempZ < mapLength - 2; ++tempZ) {
+				for (int tempX = 2; tempX < mapWidth - 2; ++tempX) {
+					if (rand.NextDouble() < landChance && GetBlock(tempX, y - 1, tempZ) > 0) {
+						temporaryMap[tempX, tempZ] = 1;
+					}
+				}
+			}
+
+			//then, simulate a 4-5 rule cellular automata 7 times
+			//for the first 4 times, also fill in areas with 0 neighbours
+
+			for (int generation = 0; generation < 5; generation++) {
+				for (int tempZ = 0; tempZ < mapLength; tempZ++) {
+					for (int tempX = 0; tempX < mapWidth; ++tempX) {
+
+						neighbourCount = GetNeighbourCount(tempX, tempZ, temporaryMap);
+
+						if (neighbourCount >= 5 || neighbourCount == 0) {
+							//testing: only fill if less than the random number
+							if (rand.NextDouble() < landChance && GetBlock(tempX, y - 1, tempZ) > 0) {
+								temporaryMap[tempX, tempZ] = 1;
+							}
+						} else {
+							temporaryMap[tempX, tempZ] = 0;
+						}
+
+					}
+				}
+			}
+
+			//for the last three times, only use the 4-5 rule
+
+			for (int generation = 0; generation < 4; generation++) {
+				for (int tempZ = 0; tempZ < mapLength; tempZ++) {
+					for (int tempX = 0; tempX < mapWidth; tempX++) {
+
+						neighbourCount = GetNeighbourCount(tempX, tempZ, temporaryMap);
+
+						if (neighbourCount >= 5) {
+							temporaryMap[tempX, tempZ] = 1;
+						} else {
+							temporaryMap[tempX, tempZ] = 0;
+						}
+
+					}
+				}
+			}
+
+			landChance -= 0.01; //make the next level more sparse
+
+			//apply the layer to the heightmap
+
+			for (int tempZ = 0; tempZ < mapLength; tempZ++) {
+				for (int tempX = 0; tempX < mapWidth; tempX++) {
+					if (temporaryMap[tempX, tempZ] == 1) {
+						mapData[tempX, y, tempZ] = 1;
+					}
+				}
+			}
+
+		}
 	}
 
-	private void CreateCubeWestFace(int x, int y, int z, byte block)
+	private int GetNeighbourCount(int x, int z, byte[,] temporaryMap)
 	{
-		// Add the new vertices
-		newVertices.Add(new Vector3(x, y - 1, z + 1));
-		newVertices.Add(new Vector3(x, y, z + 1));
-		newVertices.Add(new Vector3(x, y, z));
-		newVertices.Add(new Vector3(x, y - 1, z));
+		int neighbourCount = 0;
 
-		ApplyTextureToFace(textureStone);
+		if (z != 0 && z != (mapLength - 1) && x != 0 && x != (mapWidth - 1)) { //normal comparisons of non-border tiles
+			if (temporaryMap[x, z] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x + 1, z + 1] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x + 1, z] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x + 1, z - 1] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x, z - 1] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x - 1, z - 1] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x - 1, z] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x - 1, z + 1] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x, z + 1] == 1) {
+				neighbourCount++;
+			}
+		} else if (z == 0) {
+			if (x == 0) { //top left corner
+				if (temporaryMap[x, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z + 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x, z + 1] == 1) {
+					neighbourCount++;
+				}
+			} else if (x == mapWidth - 1) { //top right corner
+				if (temporaryMap[x, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x - 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x - 1, z + 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x, z + 1] == 1) {
+					neighbourCount++;
+				}
+			} else { //anywhere along the top border
+				if (temporaryMap[x, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z + 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x, z + 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x - 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x - 1, z + 1] == 1) {
+					neighbourCount++;
+				}
+			}
+		} else if (x == 0) {
+			if (z == mapLength - 1) { //bottom left corner
+				if (temporaryMap[x, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z - 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x, z - 1] == 1) {
+					neighbourCount++;
+				}
+			} else { //anywhere along the left border.
+				if (temporaryMap[x, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x, z - 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x, z + 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z + 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z - 1] == 1) {
+					neighbourCount++;
+				}
+			}
+		} else if (z == mapLength - 1) {
+			if (x == mapWidth - 1) { //bottom right corner
+				if (temporaryMap[x, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x - 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x - 1, z - 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x, z - 1] == 1) {
+					neighbourCount++;
+				}
+			} else { //anywhere along the bottom border
+				if (temporaryMap[x, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x, z - 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x - 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x - 1, z - 1] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z] == 1) {
+					neighbourCount++;
+				}
+				if (temporaryMap[x + 1, z - 1] == 1) {
+					neighbourCount++;
+				}
+			}
+		} else if (x == mapWidth - 1) { //anywhere along the right border
+			if (temporaryMap[x, z] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x, z + 1] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x, z - 1] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x - 1, z] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x - 1, z + 1] == 1) {
+				neighbourCount++;
+			}
+			if (temporaryMap[x - 1, z - 1] == 1) {
+				neighbourCount++;
+			}
+		}
+
+		return neighbourCount;
 	}
 
-	private void CreateCubeBottomFace(int x, int y, int z, byte block)
-	{
-		// Add the new vertices
-		newVertices.Add(new Vector3(x, y - 1, z));
-		newVertices.Add(new Vector3(x + 1, y - 1, z));
-		newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
-		newVertices.Add(new Vector3(x, y - 1, z + 1));
-
-		ApplyTextureToFace(textureStone);
-	}
-
-	private void ApplyTextureToFace(Vector2 texturePosition)
-	{
-		// Generate the triangles
-		var offset = faceCount * 4;
-
-		newTriangles.Add(offset);		// 1
-		newTriangles.Add(offset + 1); 	// 2
-		newTriangles.Add(offset + 2);	// 3
-		newTriangles.Add(offset);
-		newTriangles.Add(offset + 2);
-		newTriangles.Add(offset + 3);
-
-		var xOrigin = textureUnit * texturePosition.x;
-		var yOrigin = textureUnit * texturePosition.y;
-
-		newUV.Add(new Vector2(xOrigin, yOrigin));
-		newUV.Add(new Vector2(xOrigin + textureUnit, yOrigin));
-		newUV.Add(new Vector2(xOrigin + textureUnit, yOrigin + textureUnit));
-		newUV.Add(new Vector2(xOrigin, yOrigin + textureUnit));
-
-		++faceCount;
-	}
+	#endregion
 }
